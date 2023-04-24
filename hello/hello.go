d16a808fd10369828c45671caeaf6b4c4bc69829
ShuShu47 <98040497+ShuShu47@users.noreply.github.com>
2023-04-24 18:54:35 +0530

    Call your code from another module

--- hello/hello.go
+++ hello/hello.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 
 	"rsc.io/quote"
+
+	"greetings"
 )
 
 func main() {
@@ -11,5 +13,10 @@ func main() {
 	fmt.Println(quote.Hello())
 	fmt.Println(quote.Glass())
 	fmt.Println(quote.Opt())
-	fmt.Println("ShuShu-Day-1")
+	fmt.Println("ShuShu-Day-2")
+
+	//Get greeting module and print it.
+	message := greetings.Hello("Hello Go Gladys <Passed Arguments>")
+
+	fmt.Println(message)
 }
